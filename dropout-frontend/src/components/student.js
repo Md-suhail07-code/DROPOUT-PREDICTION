f5f852f6e6f27fda582a8a208ee406821871c0b5
Md.Suhail <suhailahmedmd40@gmail.com>
2025-09-28 20:18:44 +0530

    Update student.js

--- dropout-frontend/src/components/student.js
+++ dropout-frontend/src/components/student.js
@@ -380,20 +380,27 @@ const StudentDetail = () => {
                     ) : recommendations && recommendations.length > 0 ? (
                         <ul className="grid gap-4 mt-6">
                           {recommendations.map((item, i) => (
-                            <li key={i} className="bg-white rounded-lg p-6 shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:-translate-y-1 border border-gray-100">
-                              <div className="flex items-start">
-                                <span className="flex-shrink-0 text-blue-500 mr-4 mt-1">
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                                  </svg>
-                                </span>
-                                <p className="text-gray-800 font-medium">
-                                  {item.replaceAll('*', '').replaceAll('### ', '')}
-                                </p>
-                              </div>
-                            </li>
-                          ))}
+                            item.length < 10 ? (
+                              <h3 key={i} className="text-gray-800 font-medium">
+                                {item.replaceAll('*', '').replaceAll('### ', '')}
+                              </h3>
+                            ) : (
+                              <li key={i} className="bg-white rounded-lg p-6 shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:-translate-y-1 border border-gray-100">
+                                <div className="flex items-start">
+                                  <span className="flex-shrink-0 text-blue-500 mr-4 mt-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                                    </svg>
+                                  </span>
+                                  <p className="text-gray-800 font-medium">
+                                    {item.replaceAll('*', '').replaceAll('### ', '')}
+                                  </p>
+                                </div>
+                              </li>
+                            )
+                          ))}
                         </ul>
+
                     ) : (
                         <p className="py-4 text-center text-gray-500">No recommendations available</p>
                     )}
